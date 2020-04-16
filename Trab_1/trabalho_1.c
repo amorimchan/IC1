2254b4e9edfe9f77b0f6e54f3e915941ed214865
amorimchan <cucaratcha@icloud.com>
2020-04-16 19:28:44 -0300

    tentando colocar a adição das notas como sendo parte do menu

--- Trab_1/trabalho_1.c
+++ Trab_1/trabalho_1.c
@@ -3,75 +3,126 @@
 
 int main()
 {
-    int a, p1, p2, p3, p4, p5, i; float nota = 0, nota_1, nota_2, nota_3, nota_4, nota_5, maior = 0, menor = 0, var = 0, MA;
+    int a, i, k = 1; float maior = 0, menor = 0, var = 0, MA;
 
-    do
+    float notas[5];
+    float pesos[5];
+
+    while (k != 2)
     {
         //display das opçoes e possibilidade de escolha por parte do usuário
-        printf("Opcoes possiveis: \n");
-        printf("\n(1) Media aritmetica simples\n");
-        printf("(2) Media ponderada\n"); //colocar os pesos está no caso 2
-        printf("(3) Desvio padrao\n");
-        printf("(4) Maior e menor notas\n");
-        printf("(5) Para fechar o programa\n");
+        printf("\nSelecione uma opcao: \n");
+        printf("\n(1) Adicionar suas notas\n");
+        printf("(2) Media aritmetica simples\n");
+        printf("(3) Media ponderada\n"); //colocar os pesos está no caso 2
+        printf("(4) Desvio padrao\n");
+        printf("(5) Maior e menor notas\n");
+        printf("(6) Adicionar suas notas\n");
+        printf("(7) Para fechar o programa\n");
         printf("\nEscolha qual operacao deseja realizar: ");
         scanf("%d", &a);
 
-        //condição do maior e menor
-
-        if (a == 5)
+        if (a == 1)
         {
+            for (int i = 0; i < 5; i++)
+            {
+                printf("coloque a nota %d: ", i+1);
+                scanf("%f", &notas[i]);
+            }
+            k = 3;
             break;
         }
-        else if (a != 4)
+        else if ((k == 3)||(a == 7))
         {
-            printf("coloque as 5 notas: ");
-            scanf("%f%f%f%f%f", &nota_1, &nota_2, &nota_3, &nota_4, &nota_5);
+            k = 2;
+            break;
         }
+        else
+        {
+            printf("\n*************************************");
+            printf("\ncoloque suas notas antes de continuar\n");
+            printf("*************************************\n");
 
-    } while ((a < 1)||(a > 5));
+        }
+        
+    }
+    
 
     switch (a)
     {
-    case 1:
-        printf("A media aritmetica eh: %0.2f", (nota_1 + nota_2 + nota_3 + nota_4 + nota_5)/5);
-        break;
-
     case 2:
-        printf("Coloque os 5 pesos para as notas na respectiva ordem: ");
-        scanf("%d%d%d%d%d", &p1, &p2, &p3, &p4, &p5);
-        printf("A media ponderada eh: %0.2f", (p1*nota_1 + p2*nota_2 + p3*nota_3 + p4*nota_4 + p5*nota_5)/(p1 + p2 + p3 + p4 + p5));
+        printf("A media aritmetica eh: %0.2f", (notas[0] + notas[1] + notas[2] + notas[3] + notas[4])/5);
         break;
 
     case 3:
-        MA = (nota_1 + nota_2 + nota_3 + nota_4 + nota_5)/5;
-        printf("O desvio padrao de suas notas e' %0.3f", sqrt(pow(nota_1 - MA, 2) + pow(nota_2 - MA, 2) + pow(nota_3 - MA, 2) + pow(nota_4 - MA, 2)+ pow(nota_5 - MA, 2)));
+        for (int i = 0; i < 5; i++)
+        {
+            printf("Coloque o peso da nota %d: ", i+1);
+            scanf("%f", pesos[i]);
+        }
+        printf("A media ponderada eh: %0.2f", (pesos[0]*notas[0] + pesos[1]*notas[1] + pesos[2]*notas[2] + pesos[3]*notas[3] + pesos[4]*notas[4])/(pesos[0]+pesos[1]+pesos[2]+pesos[3]+pesos[4]));
         break;
 
     case 4:
-        printf("Coloque a nota 1: ");
-        scanf("%f", &nota);
-        menor = maior = nota;
+        MA = (notas[0] + notas[1] + notas[2] + notas[3] + notas[4])/5;
+        printf("O desvio padrao de suas notas e' %0.3f", sqrt(pow(notas[0] - MA, 2) + pow(notas[1] - MA, 2) + pow(notas[2] - MA, 2) + pow(notas[3] - MA, 2)+ pow(notas[4] - MA, 2)));
+        break;
 
-        for(i=2; i<=5; i++)
+    case 5:
+        menor = maior = notas[0];
+
+        for(i=1; i<5; i++)
         {
-            printf("Coloque a nota %d: ", i);
-            scanf("%f", &nota);
-
-            if (nota > maior)
+            if (notas[i] > maior)
             {
-                maior = nota;
+                maior = notas[i];
             }
-            /*else if (nota < maior && i == 2)
+            else if (notas[i] <= menor)
             {
-                menor = nota;
-            }*/
-            else if (nota <= menor)
-            {
-                menor = nota;
+                menor = notas[i];
             }          
         }
         printf("A maior nota e' %0.2f e a menor e' %0.2f", maior, menor);
         break;
     }
 }
+
+
+/*
+    do
+    {
+        //display das opçoes e possibilidade de escolha por parte do usuário
+        printf("\nOpcoes possiveis (suas notas serão adicionadas após selecionar uma opcao): \n");
+        printf("\n(1) Adicionar suas notas\n");
+        printf("(2) Media aritmetica simples\n");
+        printf("(3) Media ponderada\n"); //colocar os pesos está no caso 2
+        printf("(4) Desvio padrao\n");
+        printf("(5) Maior e menor notas\n");
+        printf("(6) Adicionar suas notas\n");
+        printf("(7) Para fechar o programa\n");
+        printf("\nEscolha qual operacao deseja realizar: ");
+        scanf("%d", &a);
+
+        //condição do maior e menor
+
+        if (a == 7)
+        {
+            break;
+        }
+        else if (a == 1)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                printf("coloque a nota %d: ", i+1);
+                scanf("%f", &notas[i]);
+            }
+        }
+        else
+        {
+            printf("coloque suas notas antes de continuar");
+            return;
+        }
+        
+       
+    } while ((a < 1)||(a > 7)||(a == 5));
+    */
